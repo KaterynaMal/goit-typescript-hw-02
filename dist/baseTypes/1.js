8ceb89f02ed24084716ea63582dba3fc765a6e25
KaterynaMal <96122506+KaterynaMal@users.noreply.github.com>
2024-02-10 16:11:56 +0200

    baseTypes

---
+++ dist/baseTypes/1.js
@@ -0,0 +1,9 @@
+// Перетворіть цей код на TypeScript, вказавши відповідні типи для всіх змінних.
+let age = 50;
+let name = 'Max';
+let toggle = true;
+let empty = null;
+let notInitialize;
+let callback = (a) => { return 100 + a; };
+export { age, name, toggle, empty, notInitialize, callback };
+//# sourceMappingURL=1.js.map
